93907859579375c6e4e80d97e77db92cb9feabb9
Aleksei <46716462+spartfin@users.noreply.github.com>
2019-03-22 04:26:34 +0300

    Чуть Анимации

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ var write = document.querySelector(".modal-button");
 var popup = document.querySelector(".write-to-us");
 var close = popup.querySelector(".modal-close");
 var names = popup.querySelector("[name=name]");
+var form = popup.querySelector("form");
+var email = popup.querySelector("[name=e-mail]");
+var textarea = popup.querySelector("[name=textarea]");
 
 write.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -12,6 +15,16 @@ write.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
 evt.preventDefault();
 popup.classList.remove("modal-show");
+popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!names.value || !email.value || textarea.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -19,6 +32,7 @@ if (evt.keyCode === 27) {
   evt.preventDefault();
   if (popup.classList.contains("modal-show")) {
     popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
   }
 }
 });
